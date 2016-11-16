aadafbdfd8f9dd0c30a8b9e05e9a15668e018983
Marco M <gottacatchmall@gmail.com>
2016-11-16 18:39:42 +0000

    Reordered the HoroscopeGUI class

--- client/HoroscopeGUI.java
+++ client/HoroscopeGUI.java
@@ -40,88 +40,35 @@ public class HoroscopeGUI extends JFrame
 {
 	private static final int DEFAULT_DOOR = 60000;
 	private UDPMessageSocket client;
-	private JLabel availableHoroscopeLabel;
 	private int availableHoroscopes;
 
 	public HoroscopeGUI(String username, String ipAddr, int port)
 	{
 		super("FoxHoroscope - Accesso come " + username);
-		JTextPane horoscopeArea = new JTextPane();
-		horoscopeArea.setEditable(false);
-		try
-		{
-			client = new UDPMessageSocket(DEFAULT_DOOR);
-			client.setSoTimeout(5000);
-			String[] parameters = { username };
-			client.setSoTimeout(5000);
-			client.addPacketHandler((DatagramPacket packet) ->
-			{
-				try
-				{
-					RequestMessage response = new RequestMessage(packet.getData());
-					if(response.getRequest().equals("connect"))
-					{
-						availableHoroscopes = Integer.parseInt(response.getParameter(0));
-						client.connect(ipAddr, port);
-						if(availableHoroscopes > -1)
-							availableHoroscopeLabel.setText("Hai ancora " + availableHoroscopes + " oroscopi a disposizione");
-						else
-							availableHoroscopeLabel.setText("Utente premium: numero illimitato di oroscopi");
-						setVisible(true);
-						client.setSoTimeout(5000);
-					}
-					else if(!client.isConnected())
-					{
-						JOptionPane.showMessageDialog(null, "E' accaduto un errore durante la connessione al server", "Error", JOptionPane.ERROR_MESSAGE);
-						System.exit(-1);
-					}
-				}
-				catch(InvalidMessageException ime)
-				{
-					String paragraph = new String(packet.getData()).trim();
-					horoscopeArea.setText(paragraph);
-					if(availableHoroscopes > 0)
-					{
-						availableHoroscopes--;
-						availableHoroscopeLabel.setText("Hai ancora " + availableHoroscopes + " oroscopi a disposizione");
-					}
-				}
-				catch(IOException ie) {}
-			});
-			client.addTimeoutHandler((SocketTimeoutException ste) ->
-			{
-				JOptionPane.showMessageDialog(null, "E' accaduto un errore durante la connessione al server", 
-					"Error", JOptionPane.ERROR_MESSAGE);
-				System.exit(-1);
-			});
-			client.send(new RequestMessage("connect", parameters).getBytes(), ipAddr, port);
-		}
-		catch(IOException ie)
-		{
-			JOptionPane.showMessageDialog(null, "E' accaduto un errore durante la connessione al server", 
-				"Error", JOptionPane.ERROR_MESSAGE);
-			System.exit(-1);
-		}
-
-		Border etchedBorder = BorderFactory.createEtchedBorder();
 		JPanel mainPanel = new JPanel();
+		Border etchedBorder = BorderFactory.createEtchedBorder();
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
 
-		availableHoroscopeLabel = new JLabel("Hai ancora " + availableHoroscopes + " oroscopi a disposizione");
+		JTextPane horoscopeArea = new JTextPane();
+		horoscopeArea.setEditable(false);
 		horoscopeArea.setContentType("text/html");
+
 		JPanel inputPanel = new JPanel(new GridLayout(2, 1));
 		inputPanel.setBorder(new TitledBorder(etchedBorder, "Dati utente"));
+
 		JPanel dataPanel = new JPanel(new FlowLayout());
 		String[] days = {"--", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
 					   		 "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
 					   		 "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
 		String[] months = {"--", "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
 							   "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"};
+		// creating array containing all the years starting from 1900(included)
 		ArrayList<String> yearsList = new ArrayList<String>();
 		yearsList.add("--");
 		for(int year = 2016; year >= 1900; year--)
 			yearsList.add(Integer.toString(year));
 		String[] years = yearsList.toArray(days);
+
 		JComboBox<String> dayBox = new JComboBox<String>(days);
 		dayBox.setSelectedIndex(0);
 		JComboBox<String> monthBox = new JComboBox<String>(months);
@@ -133,7 +80,9 @@ public class HoroscopeGUI extends JFrame
 		dataPanel.add(monthBox);
 		dataPanel.add(new JLabel("/"));
 		dataPanel.add(yearBox);
+
 		JPanel buttonPanel = new JPanel(new GridLayout(1, 3));
+
 		JButton sendButton = new JButton("Richiedi");
 		sendButton.addActionListener((ActionEvent e) ->
 		{
@@ -176,24 +125,26 @@ public class HoroscopeGUI extends JFrame
 		buttonPanel.add(new JLabel());
 		buttonPanel.add(sendButton);
 		buttonPanel.add(new JLabel());
+
 		inputPanel.add(dataPanel);
 		inputPanel.add(buttonPanel);
+		
 		JPanel infoPanel = new JPanel();
+
+		// "Hai ancora " + availableHoroscopes + " oroscopi a disposizione"
+
+		JLabel availableHoroscopeLabel = new JLabel();
 		infoPanel.add(availableHoroscopeLabel);
+
 		JPanel premiumPanel = new JPanel(new GridLayout(1, 2));
 		premiumPanel.setBorder(new TitledBorder(etchedBorder, "Opzioni Premium", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.CENTER));
-		URL logoPath = getClass().getResource("../logo.png");
-		JButton premiumButton = new JButton("Diventa premium!");
-		if(availableHoroscopes == -1)
-		{
-			availableHoroscopeLabel = new JLabel("Utente premium: numero illimitato di oroscopi");
-			premiumButton.setEnabled(false);
-		}
-		else
-			availableHoroscopeLabel = new JLabel("Hai ancora " + availableHoroscopes + " oroscopi a disposizione");
-		// premiumButton.setIcon(new ImageIcon(logoPath));
+		
 		JLabel logoLabel = new JLabel();
-		logoLabel.setIcon(new ImageIcon(logoPath));
+		URL logoPath = getClass().getResource("../logo.png");
+		if(logoPath != null)
+			logoLabel.setIcon(new ImageIcon(logoPath));
+
+		JButton premiumButton = new JButton("Diventa premium!");
 		premiumButton.addActionListener((ActionEvent e) ->
 		{
 			String email = JOptionPane.showInputDialog(null, "Inserisci l'email legata al tuo account PayPal: ", "PAGAH", JOptionPane.QUESTION_MESSAGE);
@@ -215,14 +166,69 @@ public class HoroscopeGUI extends JFrame
 		});
 		premiumPanel.add(premiumButton);
 		premiumPanel.add(logoLabel);
+
 		mainPanel.add(inputPanel);
 		mainPanel.add(infoPanel);
 		mainPanel.add(premiumPanel);
 		mainPanel.add(horoscopeArea);
+		
 		add(mainPanel);
+		try
+		{
+			client = new UDPMessageSocket(DEFAULT_DOOR);
+			String[] parameters = { username };
+			client.setSoTimeout(5000);
+			client.addPacketHandler((DatagramPacket packet) ->
+			{
+				try
+				{
+					RequestMessage response = new RequestMessage(packet.getData());
+					if(response.getRequest().equals("connect"))
+					{
+						availableHoroscopes = Integer.parseInt(response.getParameter(0));
+						client.connect(ipAddr, port);
+						if(availableHoroscopes > -1)
+							availableHoroscopeLabel.setText("Hai ancora " + availableHoroscopes + " oroscopi a disposizione");
+						else
+							availableHoroscopeLabel.setText("Utente premium: numero illimitato di oroscopi");
+						setVisible(true);
+						client.setSoTimeout(0);
+					}
+					else if(!client.isConnected())
+					{
+						JOptionPane.showMessageDialog(null, "E' accaduto un errore durante la connessione al server", "Error", JOptionPane.ERROR_MESSAGE);
+						System.exit(-1);
+					}
+				}
+				catch(InvalidMessageException ime)
+				{
+					String paragraph = new String(packet.getData()).trim();
+					horoscopeArea.setText(paragraph);
+					if(availableHoroscopes > 0)
+					{
+						availableHoroscopes--;
+						availableHoroscopeLabel.setText("Hai ancora " + availableHoroscopes + " oroscopi a disposizione");
+					}
+				}
+				catch(IOException ie) {}
+			});
+			client.addTimeoutHandler((SocketTimeoutException ste) ->
+			{
+				JOptionPane.showMessageDialog(null, "E' accaduto un errore durante la connessione al server", 
+					"Error", JOptionPane.ERROR_MESSAGE);
+				System.exit(-1);
+			});
+			client.send(new RequestMessage("connect", parameters).getBytes(), ipAddr, port);
+		}
+		catch(IOException ie)
+		{
+			JOptionPane.showMessageDialog(null, "E' accaduto un errore durante la connessione al server", 
+				"Error", JOptionPane.ERROR_MESSAGE);
+			System.exit(-1);
+		}
+
 		setSize(new Dimension(360, 600));
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
 	}
 }
